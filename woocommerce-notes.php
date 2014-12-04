3cadce42b684c7e4f1b5b6be2c0a3dcfc0acb822
ahmed-ibrahim <a.ibrahim@objects.ws>
2014-12-04 12:52:37 +0200

    woo notes

--- woocommerce-notes.php
+++ woocommerce-notes.php
@@ -54,3 +54,15 @@ $order->add_order_note(
 woocommerce_simple_add_to_cart();
 
 
+add_filter ('add_to_cart_redirect', 'redirect_to_checkout');
+
+function redirect_to_checkout() {
+    global $woocommerce;
+    $checkout_url = $woocommerce->cart->get_checkout_url();
+    return $checkout_url;
+}
+
+//notice
+wc_add_notice();
+wc_notice_count('error');
+wc_print_notices();
